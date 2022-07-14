d3e6cf65958b3f7e8cadce6cb8e02f8f4882ca8c
marios-dev <marios-dev@users.noreply.github.com>
2022-07-14 11:30:21 +0200

    Add project files.

---
+++ ECommerce.Api.Products/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.Api.Products.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsprovider productsProvider;
+
+        public ProductsController(IProductsprovider productsProvider)
+        {
+            this.productsProvider = productsProvider;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProductsAsync()
+        {
+            var result = await productsProvider.GetProductsAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(result.Products);
+            }
+            return NotFound();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductAsync(int id)
+        {
+            var result = await productsProvider.GetProductAsync(id);
+            if (result.isSuccess)
+            {
+                return Ok(result.Product);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
---
+++ ECommerce.Api.Products/Providers/ProductsProvider.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Providers
+{
+    public class ProductsProvider : IProductsprovider
+    {
+        private readonly ProductsDbContext dbContext;
+        private readonly ILogger<ProductsProvider> logger;
+        private readonly IMapper mapper;
+        public ProductsProvider(ProductsDbContext dbContext, ILogger<ProductsProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeedData();
+        }
+        private void SeedData()
+        {
+            if (!dbContext.Products.Any())
+            {
+                dbContext.Products.Add(new Product() { Id = 1, Name = "keyboard", Price = 20, Inventory = 100 });
+                dbContext.Products.Add(new Product() { Id = 2, Name = "Mouse", Price = 5, Inventory = 100 });
+                dbContext.Products.Add(new Product() { Id = 3, Name = "Monitor", Price = 100, Inventory = 100 });
+                dbContext.Products.Add(new Product() { Id = 4, Name = "CPU", Price = 120, Inventory = 100 });
+                dbContext.SaveChanges();
+            }
+        }
+
+        public ILogger<ProductsProvider> Logger { get; }
+        public IMapper Mapper { get; }
+
+        public async Task<(bool IsSuccess, IEnumerable<Models.Product> Products,
+            string ErrorMessage)> GetProductsAsync()
+        {
+            try
+            {
+                var products = await dbContext.Products.ToListAsync();
+                if (products != null && products.Any())
+                {
+                    var result = mapper.Map<IEnumerable<Db.Product>, IEnumerable<Models.Product>>(products);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+
+        public async Task<(bool isSuccess, Models.Product Product, string ErrorMessage)> GetProductAsync(int id)
+        {
+            try
+            {
+                var product = await dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+                if (product != null)
+                {
+                    var result = mapper.Map<Db.Product, Models.Product>(product);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
