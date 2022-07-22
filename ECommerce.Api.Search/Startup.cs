1f25f8766838659406d631e767007d309c2bf702
marios-dev <marios-dev@users.noreply.github.com>
2022-07-22 21:44:52 +0200

    created search microservice waiting for postman testing

--- ECommerce.Api.Search/Startup.cs
+++ ECommerce.Api.Search/Startup.cs
@@ -1,14 +1,10 @@
+using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ECommerce.Api.Search
 {
@@ -24,6 +20,7 @@ namespace ECommerce.Api.Search
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ISearchService, SearchService>();
             services.AddControllers();
         }
 
